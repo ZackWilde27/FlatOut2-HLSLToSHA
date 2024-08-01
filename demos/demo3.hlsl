6f2f1a6b88d166cb1b7b8bdf84186b1ebf215168
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-08-01 02:23:40 -0600

    Update demo3.hlsl
    Fixed typos

--- demos/demo3.hlsl
+++ demos/demo3.hlsl
@@ -2,8 +2,8 @@
 
 main(float3 pos : SV_Position, float3 nrm : NORMAL, float4 diff : COLOR, float2, uv1 : TEXCOORD0, uv2 : TEXCOORD1)
 {
-    tex0.uv = TEXCOORD0.xy;
-    tex1.uv = TEXCOORD1.xy;
+    tex0.uv = uv1.xy;
+    tex1.uv = uv2.xy;
 
     float4 neg1 = float4(-1.0f, -1.0f, -1.0f, 1.0f);
 
@@ -15,10 +15,10 @@ main(float3 pos : SV_Position, float3 nrm : NORMAL, float4 diff : COLOR, float2,
 // For the pixel shader, the type of the parameters is optional since they have to be float4
 psMain(tex0, tex1, tex2)
 {
-    float4 myVar = lerp(idk, thatone, thatone.a);
-    myVar = saturate(dot(idk, idk));
-    myVar = saturate(myVar + idk);
-    float4 l = theseVariablesCanBeNamedAnythingOnlyTheOrderDeterminesWhichTextureTheyAre * SHADOW;
+    float4 myVar = lerp(tex0, tex1, tex1.a);
+    myVar = saturate(dot(tex0, tex0));
+    myVar = saturate(myVar + tex0);
+    float4 l = tex2 * SHADOW;
     l = saturate(l + AMBIENT);
     return myVar * l;
 }
