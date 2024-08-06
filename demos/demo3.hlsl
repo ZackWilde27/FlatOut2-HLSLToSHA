8b96a393061c111426b9c41f4f55bf2bc13c9395
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-08-06 17:30:46 -0600

    Update demo3.hlsl
    Fixed Typo

--- demos/demo3.hlsl
+++ demos/demo3.hlsl
@@ -2,8 +2,8 @@
 
 main(float3 pos : SV_Position, float3 nrm : NORMAL, float4 diff : COLOR, float2, uv1 : TEXCOORD0, uv2 : TEXCOORD1)
 {
-    tex0.uv = TEXCOORD0.xy;
-    tex1.uv = TEXCOORD1.xy;
+    tex0.uv = uv1.xy;
+    tex1.uv = uv2.xy;
 
     float4 neg1 = float4(-1.0f, -1.0f, -1.0f, 1.0f);
 
