ca80dc20fa247b415712e67e000d323a1db3a054
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-08-24 21:32:35 -0600

    FlatOutShaderToHLSL v1.1
    I didn't think I'd have to update it so soon but I noticed some oversights. I added some more features while I was at it
    
    - Unknown lines are now wrapped with asm {}, I accidentally had them discarded
    
    - I forgot the matrix instructions, those have been added
    
    - It can handle one or both of the shaders being missing, so it can now decompile emu_ shaders as well
    
    - Added a label for the return value above the function, I couldn't get it to insert returns bug-free, I'll have to spend more time on that

--- SHADecompiler.py
+++ SHADecompiler.py
@@ -1,5 +1,5 @@
 # Zack's FlatOut SHA To HLSL
-# Version 1.0
+# Version 1.1
 # Am I particularly proud of this code? uhh
 
 try:
@@ -37,13 +37,15 @@ pixelshaderconsts = []
 
 isPixelShader = False
 
-vsDefines = [("oD0.rgb", "AMBIENT"), ("oD0.a", "FRESNEL"), ("oD1.rgb", "EXTRA"), ("oD1.a", "BLEND"), ("c8", "CAMERA")]
+vsDefines = [("oD0.xyz", "AMBIENT"), ("oD0.w", "FRESNEL"), ("oD1.xyz", "EXTRA"), ("oD1.w", "BLEND"), ("c8", "CAMERA")]
 psDefines = [("v0", "AMBIENT"), ("v0.a", "FRESNEL"), ("v1", "EXTRA"), ("v1.a", "BLEND"), ("c2", "SHADOW")]
 
 def DecompileOperand(string):
     string = string.strip()
     if not string:
         return ""
+
+    print(string)
 
     swizzle = ""
     if "." in string:
@@ -66,9 +68,12 @@ def DecompileOperand(string):
         if string == d[0]:
             return d[1] + swizzle
 
+    print(isPixelShader, string)
     # Parameters
     if isPixelShader:
         if string[0] == "t":
+            if len(pixelshaderinputs) == 1:
+                return pixelshaderinputs[0] + swizzle
             return pixelshaderinputs[int(string[1])].split(" ")[-1] + swizzle
     else:
         if string[0] == "v":
@@ -77,16 +82,19 @@ def DecompileOperand(string):
                 if i.split(" ")[-1] == selection:
                     return i.split(" ")[-3] + swizzle
 
-    if string[:2] == "oT":
-        if "z" not in swizzle:
-            swizzle = swizzle.replace("x", "u").replace("y", "v")
-        return pixelshaderinputs[int(string[2])].split(" ")[1] + swizzle
+        if string[:2] == "oT":
+            if "z" not in swizzle:
+                swizzle = swizzle.replace("x", "u").replace("y", "v")
+            if pixelshaderinputs:
+                return pixelshaderinputs[int(string[2])].split(" ")[1] + swizzle
+            else:
+                return "tex_" + str(int(string[2]))
 
     return "\"" + string + swizzle + "\""
         
 mods = [("sat", "saturate"), ("x2", "x2"), ("d2", "d2"), ("x4", "x4")]        
 
-def Decompile(script):
+def Decompile(script, dst):
     
     output = ""
     for line in script.split("\n"):
@@ -101,9 +109,9 @@ def Decompile(script):
         line = line.strip()
 
         if line:
+            handled = False
             if "\t" in line:
                 if line[0] == "+":
-                    print(line)
                     prefix = "meanwhile "
                     line = line[1:]
 
@@ -129,66 +137,80 @@ def Decompile(script):
                                     functionsE += ")"
                 
                 instruction = line[:line.index("\t")]
+                arguments = line[line.index("\t") + 1:].split(",")
+                arguments = [item.strip() for item in arguments]
+                
+                # Checking for m[n]x[n]
+                if len(instruction) == 4 and instruction[1] in "0123456789" and instruction[3] in "0123456789":
+                    matrixFuncs = [("c0", "WorldToScreen"), ("c4", "LocalToWorld")]
+                    for m in matrixFuncs:
+                            if arguments[-1] == m[0]:
+                                output += DecompileOperand(arguments[0]) + " = " + functionsB + m[1] + "(" + DecompileOperand(arguments[-2]) + ");\n"
+                                handled = True
+                                break
+
                 for i in intrinsic:
                     if i[0] == instruction:
-                        arguments = line[line.index("\t") + 1:].split(",")
-                        arguments = [item.strip() for item in arguments]
                         output += prefix + DecompileOperand(arguments[0]) + " = " + functionsB + i[1] + functionsE + ";\n"
                         for dex, arg in enumerate(arguments[1:]):
                             output = output.replace("%" + str(dex), DecompileOperand(arg))
-                        continue
-                if instruction == "mov":
-                    arguments = line[line.index("\t") + 1:].split(",")
-                    arguments = [item.strip() for item in arguments]
-                    
-                    output += prefix + DecompileOperand(arguments[0]) + " = " + functionsB + DecompileOperand(arguments[1]) + functionsE + ";\n"
-                #else:
-                    #print("Unknown line: " + line)
-                    
+                        handled = True
+                        break
+
+                if not handled:
+                    if instruction == "mov":
+                        arguments = line[line.index("\t") + 1:].split(",")
+                        arguments = [item.strip() for item in arguments]
+                        
+                        output += prefix + DecompileOperand(arguments[0]) + " = " + functionsB + DecompileOperand(arguments[1]) + functionsE + ";\n"
+                    else:
+                        if line.split("\t")[0] not in ["tex", "dcl"]:
+                            output += "asm { " + line + " }\n"
+
     return '\n'.join(["\t" + item for item in output.split("\n")])[:-2]
     
 with open(filename) as aFile:
     assembly = aFile.read()
-    #print(assembly)
     vertexshader = GetShader(assembly, "vSdr")
     pixelshader = GetShader(assembly, "pSdr")
 
-    vDecl = vertexshader[vertexshader.index("{") + 1:vertexshader.index("}")].strip()
+    if vertexshader != "Nope":
+        vDecl = vertexshader[vertexshader.index("{") + 1:vertexshader.index("}")].strip()
+        vertexshader = vertexshader[vertexshader.index("asm"):]
+        if "}" in vertexshader:
+            vertexshader = vertexshader[vertexshader.index("{") + 1:vertexshader.index("}")]
+        else:
+            vertexshader = vertexshader[vertexshader.index("{") + 1:]
 
-    vertexshader = vertexshader[vertexshader.index("asm"):]
-    if "}" in vertexshader:
-        vertexshader = vertexshader[vertexshader.index("{") + 1:vertexshader.index("}")]
-    else:
-        vertexshader = vertexshader[vertexshader.index("{") + 1:]
+        vertexshader = vertexshader.strip()
+        vertexshaderinputs = []
+        
+        for decl in vDecl.split("\n"):
+            decl = decl.strip()
+            if "v" in decl:
+                dimensions = 1
+                if "[" in decl:
+                    dimensions = int(decl[decl.index("[") + 1])
 
-    vertexshader = vertexshader.strip()
-    pixelshader = pixelshader[pixelshader.index("{") + 1:].strip()
+                vertexshaderinputs.append("float" + (str(dimensions) if dimensions > 1 else "") + " " + semanticNames[int(decl[decl.index("v") + 1])] + " : " + semantics[int(decl[decl.index("v") + 1])])
 
-    vertexshaderinputs = []
+    if pixelshader != "Nope":
+        pixelshader = pixelshader[pixelshader.index("{") + 1:].strip()
 
-    for decl in vDecl.split("\n"):
-        decl = decl.strip()
-        if "v" in decl:
-            dimensions = 1
-            if "[" in decl:
-                dimensions = int(decl[decl.index("[") + 1])
-            vertexshaderinputs.append("float" + (str(dimensions) if dimensions > 1 else "") + " " + semanticNames[int(decl[decl.index("v") + 1])] + " : " + semantics[int(decl[decl.index("v") + 1])])
+        pixelshaderinputs = []
 
-
-    pixelshaderinputs = []
-
-    for line in pixelshader.split("\n"):
-        if line.strip()[:3] == "tex":
-            if "//" in line:
-                line = line[line.index("//") + 2:]
-            elif ";" in line:
-                line = line[line.index(";") + 1:]
-            else:
-                line = "tex_" + str(len(pixelshaderinputs) + 1)
-            line = line.strip()
-            if " " in line:
-                line = line[:line.index(" ")]
-            pixelshaderinputs.append("float4 " + line)
+        for line in pixelshader.split("\n"):
+            if line.strip()[:3] == "tex":
+                if "//" in line:
+                    line = line[line.index("//") + 2:]
+                elif ";" in line:
+                    line = line[line.index(";") + 1:]
+                else:
+                    line = "tex_" + str(len(pixelshaderinputs) + 1)
+                line = line.strip()
+                if " " in line:
+                    line = line[:line.index(" ")]
+                pixelshaderinputs.append("float4 " + line)
 
     passText = assembly[assembly.index("Pass P0"):]
     passText = passText[passText.index("{") + 1:passText.index("}")]
@@ -196,13 +218,15 @@ with open(filename) as aFile:
     for line in passText.split("\n"):
         if "PixelShaderConstantF" in line:
             pixelshaderconsts.append(("c" + line[line.index("[") + 1:line.index("]")], ))
+
+    
             
     with open(filename[:filename.index(".")] + "_decompiled.hlsl", "w") as newFile:
         newFile.write("// " + filename[filename.rfind("/") + 1:filename.index(".")] + " decompiled\n")
-        newFile.write("// Zack's FlatOut-Shader-To-HLSL v1.0\n\n")
+        newFile.write("// Zack's FlatOut-Shader-To-HLSL v1.1\n\n")
         isPixelShader = False
-        newFile.write("float4 VertexShader(" + ", ".join(vertexshaderinputs) + ")\n{\n" + Decompile(vertexshader) + "\n}\n\n")
+        newFile.write("// Return value: \"oPos\"\nfloat4 VertexShader(" + ", ".join(vertexshaderinputs) + ")\n{\n" + Decompile(vertexshader, "\"oPos\"") + "\n}\n\n")
         isPixelShader = True
-        newFile.write("float4 PixelShader(" + ", ".join(pixelshaderinputs) + ")\n{\n" + Decompile(pixelshader) + "\n}")
+        newFile.write("//Return value: var1\nfloat4 PixelShader(" + ", ".join(pixelshaderinputs) + ")\n{\n" + Decompile(pixelshader, "var1") + "\n}")
 
 print("Done!")
