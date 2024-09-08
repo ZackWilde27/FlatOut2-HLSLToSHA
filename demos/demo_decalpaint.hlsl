3da4e2e10c143cf96ec2461fcd37fabc4f4e2241
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-09-08 12:53:23 -0600

    Update demo_decalpaint.hlsl
    Updated to v2.1

--- demos/demo_decalpaint.hlsl
+++ demos/demo_decalpaint.hlsl
@@ -9,16 +9,17 @@ float4 VertexShader(float3 pos : SV_Position, float2 uv1 : TEXCOORD0, float2 uv2
     tex0.uv = uv1.xy;
     tex1.uv = uv2.xy;
 
-    return WorldToScreen(pos);
+    // clip 0
+    // float4 var1 = dot(pos, clip0.xyz);
+    // float4 var2 = dot(pos, clip1.xyz);
+    // tex2.x = var1.x + clip0.w;
+    // tex2.y = var2.y + clip1.w;
+    // tex2.z = pos.z - pos.z;
+
+    return LocalToScreen(pos);
 }
 
-float4 PixelShader(float4 tex0, float4 tex1)
+float4 PixelShader(float4 tex0, float4 tex1, texkill tex2)
 {
-    // I don't have any way to texkill, so put it in assembly for now
-    asm
-    {
-        texkill     t2
-    }
-
     return tex0 * tex1;
 }
