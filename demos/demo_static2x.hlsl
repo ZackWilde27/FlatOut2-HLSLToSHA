d19c8697806e723b947bc04d91cb252afb8caffa
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-09-08 12:59:59 -0600

    Update demo_static2x.hlsl

--- demos/demo_static2x.hlsl
+++ demos/demo_static2x.hlsl
@@ -1,12 +1,12 @@
 // static2x template
 
-float4 VertexShader(float3 pos : SV_Position, float4 diff : COLOR, float2 uv : TEXCOORD)
+float4 VertexShader(float3 pos : POSITION, float4 diff : COLOR, float2 uv : TEXCOORD)
 {
     AMBIENT = diff.xyz
     FRESNEL = diff.a;
     colour.uv = uv.uv;
     
-    return WorldToScreen(pos);
+    return LocalToScreen(pos);
 }
 
 float4 PixelShader(colour)
