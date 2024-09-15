428cb95fbc060a6a7804738530691a2e8f698d8d
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-09-15 15:32:08 -0600

    Update demo_car_body.hlsl

--- demos/demo_car_body.hlsl
+++ demos/demo_car_body.hlsl
@@ -48,7 +48,7 @@ float4 psMainD3D9(float4 colour, float4 specular, float4 dirt, float4 lighting)
 {
     float4 c = specular * FRESNEL;
     c = saturate(c + colour);
-    float4 l = lighting * SHADOW;
+    float4 l = lighting.a * SHADOW;
     l = saturate(l + AMBIENT);
     return c * l;
 }
