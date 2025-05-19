f17fd1811c199773a7f68b134d5adda5c16a11f2
Zack <115175938+ZackWilde27@users.noreply.github.com>
2025-05-19 02:31:25 -0600

    Another post processing shader template

---
+++ demos/demo_post_highpass_luminance.hlsl
@@ -0,0 +1,29 @@
+// post_highpass_luminance template
+// Original shader from the game, with the original comments too
+
+#pconstants 6
+
+// There's still a bug where you can't add a semi-colon to the end of the inputStreamFormat, I'll fix that tomorrow
+const string inputStreamFormat = "PosprojTex4"
+
+#define weights "c5"
+#define scale "c4"
+#define cutoff "c3"
+
+float4 PixelShader(float4 colour)
+{
+    float4 lum;
+
+    // OLD: Convert to luminance (C5 = RGB component weights)
+    //lum = dot(colour, weights);
+
+    // luminance should already be in the source alpha, just copy to all components
+    lum.rgb = colour.a;
+    meanwhile lum.a = colour.a;
+
+    // Cut-off
+    lum -= cutoff;
+
+    // Scaling
+    return lum * scale * 4;
+}
